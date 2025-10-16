2503160b628caa34dad18f1c70f99764500f2b47
Jayaseela pandian A <jayaseelapandian44@gmail.com>
2025-10-17 00:54:27 +0530

    Add files via upload

---
+++ muxx_be_.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/17/2025 12:53:15 AM
+// Design Name: 
+// Module Name: muxx_be_
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module muxx_be_(
+
+    );
+endmodule
